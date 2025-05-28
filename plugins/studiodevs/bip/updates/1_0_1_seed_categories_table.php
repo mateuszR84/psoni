b1614ce0360d58e013bef62b572271c73999bab2
Mateusz Rycombel <mateusz@studiodevs.pl>
2025-05-28 20:30:02 +0200

    seed categories

--- plugins/studiodevs/bip/updates/1_0_1_seed_categories_table.php
+++ plugins/studiodevs/bip/updates/1_0_1_seed_categories_table.php
@@ -4,33 +4,22 @@ namespace StudioDevs\Bip\Updates\Seeds;
 
 use Seeder;
 use StudioDevs\Bip\Models\Category;
-use Illuminate\Support\Str;
 
 class CategorySeeder extends Seeder
 {
     public function run()
     {
-        $categories = [
-            'Status prawny/forma prawna',
-            'Organizacja',
-            'Przedmiot działalności',
-            'Organy i osoby sprawujące funkcje i ich kompetencje',
-            'Struktura organizacyjna i własnościowa',
-            'Majątek',
-            'Tryb działania',
-            'Sposoby przyjmowania i załatwiania spraw',
-            'Informacje o prowadzonych rejestrach, ewidencjach i archiwach oraz o sposobach i zasadach udostępniania danych w nich zawartych',
-            'Nabór kandydatów do zatrudnienia na wolne stanowiska',
-        ];
+        $categoriesFilePath = plugins_path('studiodevs/bip/updates/categories.json');
+        $categoriesData = json_decode(file_get_contents($categoriesFilePath), true);
 
-        foreach ($categories as $name) {
-            Category::updateOrCreate(
-                ['slug' => Str::slug($name)],
-                [
-                    'name' => $name,
-                    'page_code' => 'orew',
-                ]
-            );
+        foreach ($categoriesData as $slug => $categoryData) {
+            $category = new Category();
+            $category->slug = $slug;
+            $category->name = $categoryData['name'];
+            $category->page_code = $categoryData['page_code'];
+            $category->menu_order = $categoryData['menu_order'];
+            $category->description = $categoryData['description'];
+            $category->save();
         }
     }
 }
